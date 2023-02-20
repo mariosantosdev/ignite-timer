09a2039fb64052ecdd3917fcbc34ebbe26776ca9
Mário Santos <mariodev7@gmail.com>
2023-02-20 18:21:58 -0300

    setup initial vite

---
+++ src/styles/global.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    :focus {
+        outline: 0;
+        box-shadow: 0 0 0 2px ${({ theme }) => theme.colors["green-500"]};
+    }
+
+    body {
+        background: ${({ theme }) => theme.colors["gray-900"]};
+        color: ${({ theme }) => theme.colors["gray-300"]};   
+    }
+
+    body, input, button, textarea { 
+        font: 400 1rem 'Roboto', sans-serif;
+    }
+`;
